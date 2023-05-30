39a118185d86418e28a450f0a04dd13b388f942d
z14git <lzl1992@gmail.com>
2023-05-30 14:08:45 +0800

    add 检测数据集功能

--- tools/split_dataset.py
+++ tools/split_dataset.py
@@ -2,16 +2,30 @@ import os
 import random
 import argparse
 
+
+def check_dataset(src_dir):
+    if not os.path.exists(src_dir):
+        raise FileNotFoundError(f'原始数据集文件夹 "{src_dir}" 不存在')
+
+    jpg_files = [f for f in os.listdir(src_dir) if f.endswith('.jpg')]
+    xml_files = [f for f in os.listdir(src_dir) if f.endswith('.xml')]
+
+    if not jpg_files or not xml_files:
+        raise FileNotFoundError(f'原始数据集文件夹 "{src_dir}" 中缺少 jpg 或 xml 文件')
+
+    return jpg_files
+
+
 def split_dataset(src_dir, output_dir, split_ratio):
     train_dir = os.path.join(output_dir, 'train')
     val_dir = os.path.join(output_dir, 'val')
 
+    # 检查原始数据集并获取所有 jpg 文件列表
+    jpg_files = check_dataset(src_dir)
+
     # 创建训练集和验证集目录
     os.makedirs(train_dir, exist_ok=True)
     os.makedirs(val_dir, exist_ok=True)
-
-    # 获取所有 jpg 文件列表
-    jpg_files = [f for f in os.listdir(src_dir) if f.endswith('.jpg')]
 
     # 打乱文件列表并按比例划分训练集和验证集
     random.shuffle(jpg_files)
@@ -37,11 +51,15 @@ def split_dataset(src_dir, output_dir, split_ratio):
 
     print("数据集划分完成。")
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='将数据集分为训练集和验证集')
     parser.add_argument('src_dir', help='原始数据集文件夹路径')
     parser.add_argument('output_dir', help='train 和 val 文件夹的父目录')
-    parser.add_argument('--split_ratio', type=float, default=0.8, help='训练集所占比例，默认为 0.8')
+    parser.add_argument('--split_ratio',
+                        type=float,
+                        default=0.8,
+                        help='训练集所占比例，默认为 0.8')
 
     args = parser.parse_args()
 
